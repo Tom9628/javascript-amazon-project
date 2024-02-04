5619fdbb7afda8042e501b669ee56cffc5f7f89f
Tom9328 <compro.xwei@gmail.com>
2024-02-04 08:35:27 -0600

    Make delivery options active

--- data/cart.js
+++ data/cart.js
@@ -49,3 +49,14 @@ export function removeFromCart(productId){
     cart = newCart
     saveToStorage()
 }
+
+export function updateDeliveryOption(productId,deliveryOptionId){
+    let matchingItem;
+    cart.forEach((item)=>{
+        if(item.productId === productId){
+            matchingItem = item
+        }
+    })
+    matchingItem.deliveryOptionId = deliveryOptionId
+    saveToStorage()
+}
